8ee2ed98ec76d722ded184936e33f76858a8f969
no-ikjun <choiikjun1101@gmail.com>
2023-03-31 00:09:41 +0900

    delete rest files

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,6 +4,10 @@ import { BoardsService } from './boards.service';
 
 @Module({
   controllers: [BoardsController],
-  providers: [BoardsService]
+  providers: [BoardsService],
 })
 export class BoardsModule {}
+
+//따라하면서 배우는 NestJS
+//https://www.youtube.com/watch?v=3JminDpCJNE
+//28:49
